29a42e369f2d0404d425797a4c5e22a709a35cb7
theTY2002 <71263782+theTY2002@users.noreply.github.com>
2026-01-24 15:45:32 -0800

    GitHub Pages deployment

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -9,6 +9,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 export default defineConfig({
+  base: '/react-portfolio/',
   plugins: [
     react(),
     tailwindcss()
