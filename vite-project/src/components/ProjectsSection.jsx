ad6f179fa807ee47d5d6c63f4e78d7fd3a0296da
theTY2002 <71263782+theTY2002@users.noreply.github.com>
2026-01-24 16:32:41 -0800

    GitHub Pages deployment

--- vite-project/src/components/ProjectsSection.jsx
+++ vite-project/src/components/ProjectsSection.jsx
@@ -42,7 +42,7 @@ export const ProjectsSection = () =>
             <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center"> Featured<span className="text-primary"> Projects</span></h2>
 
             <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
-                Here are some of my recent projects. BLAH BLAH BLAH
+                Here are some of my recent projects.
             </p>
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
